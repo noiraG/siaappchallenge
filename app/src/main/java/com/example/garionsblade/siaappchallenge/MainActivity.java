9737a0c9f6496fe5808566abb41da6dd89e980ef
Garion's Blade <yuxuanng@hotmail.com>
2017-10-20 00:23:26 +0800

    Garion

--- app/src/main/java/com/example/garionsblade/siaappchallenge/MainActivity.java
+++ app/src/main/java/com/example/garionsblade/siaappchallenge/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.garionsblade.siaappchallenge;
 
+import android.content.Intent;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,17 +8,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+
 public class MainActivity extends AppCompatActivity {
     MainActivity currentScreen = this;
+    Button myButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myButton = (Button) findViewById(R.id.button2);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(currentScreen, MapsActivity.class);
+                startActivity(myIntent);
+            }
+
+        });
     }
-
-    EditText myEditText = (EditText) findViewById(R.id.editText);
-    Button myButton = (Button) findViewById(R.id.button);
-
-
-
 }
---
+++ app/src/main/java/com/example/garionsblade/siaappchallenge/MapsActivity.java
@@ -0,0 +1,68 @@
+package com.example.garionsblade.siaappchallenge;
+
+import android.content.res.Resources;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.GroundOverlayOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    private static String TAG = "error";
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        GroundOverlayOptions overlay1 = new GroundOverlayOptions();
+        BitmapDescriptor image = ;
+
+
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.style_json));
+
+                if (!success) {
+                    Log.e(TAG, "Style parsing failed.");
+                }
+            } catch (Resources.NotFoundException e) {
+                Log.e(TAG, "Can't find style. Error: ", e);
+        }
+
+        // Add a marker in Sydney and move the camera
+        LatLng singapore = new LatLng(1.3518, 103.8198);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(singapore));
+    }
+}
