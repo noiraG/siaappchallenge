df4e780aeed7d05128653ce64cad21028b09193c
Garion's Blade <yuxuanng@hotmail.com>
2017-10-19 12:41:03 +0800

    Garion

--- app/src/main/java/com/example/garionsblade/siaappchallenge/MainActivity.java
+++ app/src/main/java/com/example/garionsblade/siaappchallenge/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.garionsblade.siaappchallenge;
 
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    MainActivity currentScreen = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    Button myButton = (Button) findViewById(R.id.button);
+
     }
 }
